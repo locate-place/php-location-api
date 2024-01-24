fe93701f49d60e4c285f0fafedbb7bdadc3ea110
Björn Hempel <bjoern@hempel.li>
2024-01-25 00:14:58 +0100

    Add elevation to relavance search and mountains

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -672,7 +672,7 @@ class Location
             FeatureCodeConstants::MT,
             FeatureCodeConstants::PK,
         ])) {
-            $relevance += (int) round($elevation * 50000);
+            $relevance += (int) round($elevation * 100000);
         }
 
         if (is_null($coordinate)) {
